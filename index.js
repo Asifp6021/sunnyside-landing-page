d94f667cf15067963c76e0a4c171aeda4b1a7154
Asif-patel <asifp6021@gmail.com>
2024-02-21 13:00:36 +0530

    styling done

---
+++ index.js
@@ -0,0 +1,24 @@
+const navLinks = document.querySelectorAll('.header__nav--link');
+const hamburgerMenu = document.querySelector('.hamburger');
+const navContainer = document.querySelector('.header__content--nav');
+
+navLinks.forEach(function (nav) {
+	nav.addEventListener('click', function () {
+		navLinks.forEach(function (activeLink) {
+			activeLink.classList.remove('active');
+			nav.classList.add('active');
+		});
+
+		if (nav.classList.contains('.header__nav--link')) {
+			nav.classList.remove('header__nav--link');
+			nav.classList.add('active');
+		}
+		navContainer.classList.add('show');
+	});
+});
+
+console.log('hello');
+
+hamburgerMenu.addEventListener('click', function () {
+	navContainer.classList.toggle('show');
+});
---
+++ newFile.js
@@ -0,0 +1,9 @@
+navLinks.forEach(function (navLinks) {
+	navLinks.addEventListener('click', function () {
+		if (navLinks.classList.contains('.header__nav--link')) {
+			navLinks.remove.classList('.header__nav--link');
+		}
+	});
+
+	navLinks.add;
+});
